51ae96a288091e246519f36f3c44a5cfb2ad9f50
周萍 <zhouping@ebigdata.org>
2018-12-20 10:13:09 +0800

    fix:测试环境配置修改，用于测试部测试

--- src/main/java/com/xdbigdata/app_center/common/Contation.java
+++ src/main/java/com/xdbigdata/app_center/common/Contation.java
@@ -9,8 +9,9 @@ public class Contation {
     //用户管理信息是否修改参数
     public static final Integer BACIS=1;
     //跳转地址
-    public static final String URL="http://www.zhxg.zjut.edu.cn/information-management/";
+//    public static final String URL="http://www.zhxg.zjut.edu.cn/information-management/";
     //public static final String URL="http://192.168.3.91:8080/";
+    public static final String URL="http://zjutzhxg.xdbigdata.com:17080/wisdom-platform/";
 
     public static final String STUDENT="学生";
 }
