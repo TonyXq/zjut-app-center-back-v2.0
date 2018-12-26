ba9cd1bbbcd8bef3ea254e9c48b4a2f18ee247e1
tangyijun <tangyijun@ebigdata.org>
2018-12-26 14:43:58 +0800

    修改不能匹配角色的bug

--- src/main/java/com/xdbigdata/app_center/web/AppController.java
+++ src/main/java/com/xdbigdata/app_center/web/AppController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by xdpc111 on 2017/9/6.
@@ -35,6 +36,9 @@ public class AppController {
         RoleAndInfoDto roleAndInfoDtoInSession = (RoleAndInfoDto) httpSession.getAttribute("sessionUser");
         //根据前端传递的角色确定参数
         List<Role> roles = roleAndInfoDto.getRoleList();
+        //查询到选择的selectedRolesNames
+        List<String> selectedRolesNames = roles.stream().map(r -> r.getName()).collect(Collectors.toList());
+        List<String> roleNames = roleAndInfoDtoInSession.getRoleList().stream().map(r -> r.getName()).collect(Collectors.toList());
         // 验证角色集合有没有(如果没有,说明修改了参数)
         // 验证选择的角色集合是否大于1(如果大于1,说明不是正常操作,修改了参数)
         // 验证用户的session中角色是否为null或者没有,如果没有,提示需要增加角色.
@@ -45,8 +49,8 @@ public class AppController {
             return new CommonResult(false, null, ErrorCode.HANDLER_FAILED.code, "角色只能选择一个");
         }else if (CollectionUtils.isEmpty(roleAndInfoDtoInSession.getRoleList())){
             return new CommonResult(false,null,ErrorCode.HANDLER_FAILED.code,"当前用户没有角色");
-        }else if(!roleAndInfoDtoInSession.getRoleList().containsAll(roles)){
-            return new CommonResult(false,null,ErrorCode.HANDLER_FAILED.code,"选择的角色不在当前用户角色信息中");
+        }else if(!roleNames.containsAll(selectedRolesNames)){
+            return new CommonResult(false,null,ErrorCode.HANDLER_FAILED.code,"选择的角色"+selectedRolesNames+"不在当前用户角色信息中");
         }
         //把角色集合重新设值到session中
         ArrayList<Role> selectedRoles = new ArrayList<>();
